2cf2bc9711ffa741e18191203722ab3d896c7bc8
Aarav Vishnoi <aaravvishnoi@MacBook-Pro-2.local>
2026-04-26 21:13:54 +1000

    Constructor created for Population class

--- src/Population.java
+++ src/Population.java
@@ -1,3 +1,16 @@
+import java.util.*;
 class Population{
+
+    //Holds arraylist of all creatures of one species
+    private ArrayList<Creature> allCreatures;
+    private Species species;
+
+    public Population(Species species){
+        this.species = species;
+        this.allCreatures = new ArrayList<>();
+    }
+
+
+    
     
 }
