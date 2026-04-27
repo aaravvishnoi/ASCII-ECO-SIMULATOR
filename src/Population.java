1ae1b5437feb64c3454f325e2d0b7ff534b63078
Aarav Vishnoi <aaravvishnoi@MacBook-Pro-2.local>
2026-04-27 13:49:05 +1000

    Added avgStats() method to Population

--- src/Population.java
+++ src/Population.java
@@ -21,7 +21,7 @@ class Population {
                 int new_speed = mutate(allCreatures.get(i).getSpeed());
                 int new_Vision = mutate(allCreatures.get(i).getVision());
                 Creature child = new Creature(new_speed, new_def, new_strenght, new_Vision, allCreatures.get(i),
-                species.getSpeciesName(), 100, species);
+                        species.getSpeciesName(), 100, species);
                 newborn.add(child);
             }
         }
@@ -39,9 +39,9 @@ class Population {
         return stat;
     }
 
-    public void cull(){
+    public void cull() {
         ArrayList<Creature> dead = new ArrayList<>();
-        for(int i=0; i < allCreatures.size(); i++){
+        for (int i = 0; i < allCreatures.size(); i++) {
             if (allCreatures.get(i).getHealth() <= 0 || allCreatures.get(i).getAge() > species.getMaxAge()) {
                 dead.add(allCreatures.get(i));
             }
@@ -49,5 +49,29 @@ class Population {
         allCreatures.removeAll(dead);
     }
 
+    public void avgStats() {
 
+        if(allCreatures.size() ==0){
+            System.out.println("No creatures in population");
+        }
+
+
+        double avg_def = 0;
+        double avg_str = 0;
+        double avg_vis = 0;
+        double avg_speed = 0;
+        for (int i = 0; i < allCreatures.size(); i++) {
+            avg_def += allCreatures.get(i).getDefence();
+            avg_str += allCreatures.get(i).getStrength();
+            avg_vis += allCreatures.get(i).getVision();
+            avg_speed += allCreatures.get(i).getSpeed();
+        }
+        avg_def = avg_def/allCreatures.size();
+        avg_speed = avg_speed/allCreatures.size();
+        avg_str = avg_str/allCreatures.size();
+        avg_vis = avg_vis/allCreatures.size();
+
+        System.out.println(species.getSpeciesName()+"|"+avg_def+"|"+avg_str+"|"+avg_vis+"|"+avg_speed);
+
+    }
 }
