29287cc3dad9fca8f7b6cb7d785dcf7c122358f5
Aarav Vishnoi <aaravvishnoi@MacBook-Pro-2.local>
2026-04-27 16:00:21 +1000

    Added getter for Species

--- src/Ecosystem.java
+++ src/Ecosystem.java
@@ -1,3 +1,37 @@
-class Ecosystem{
-    
+import java.util.*;
+
+class Ecosystem {
+
+    private ArrayList<Population> population;
+
+    public Ecosystem() {
+        this.population = new ArrayList<>();
+
+    }
+
+    public void addPopulation(Population p) {
+        population.add(p);
+    }
+
+    void runGeneration() {
+
+    }
+
+    void plantGrowth() {
+        for (int i = 0; i < population.size(); i++) {
+            if (population.get(i).getSpeciesName().equals("Plant")) {
+                int currentSize = population.get(i).getPopulationCount();
+                int maxCarry = population.get(i).getSpecies().getMaxCarry();
+                if(currentSize > maxCarry){
+                    // new plant can be added
+                    int growthRate =5;
+                    for(int j=0; j < growthRate && currentSize + j < maxCarry; j++){
+                        population.get(i).getAllCreatures().add(new Creature(0, 5, 0, 0, null, "Plant", 100,population.get(i).getSpecies()));
+                        
+                    }
+
+            }
+        }
+    }
+
 }
