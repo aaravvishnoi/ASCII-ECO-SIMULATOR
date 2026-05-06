8ce1149f38bf52bff4b9b6224c22f724360bd409
Aarav Vishnoi <aaravvishnoi764@gmail.com>
2026-05-06 18:24:53 +1000

    Added printLineage and getAncestors method to Family tree, code is still redundant, need to fix

--- src/FamilyTree.java
+++ src/FamilyTree.java
@@ -1,3 +1,31 @@
-class FamilyTree{
-    
+import java.util.ArrayList;
+class FamilyTree {
+    public int getDepth(Creature creature, int depth) {
+        // Implementation for getting the depth of a creature in the family tree
+        // base case if((c.getParent() != null, continue))
+        // recursive case: call getDepht again but on c.getParent() and increment by 1;
+        if (creature.getParent() == null) {
+            return depth;
+        }
+        return getDepth(creature.getParent(), depth+1);
+    }
+
+
+    public String printLineage(Creature creature){
+        //should recurisvely print each anecestor name up the chain
+        if (creature.getParent() == null) {
+            return creature.getName();
+        }
+        return creature.getName() + " -> " + printLineage(creature.getParent());
+    }
+
+    public ArrayList<Creature> getAncestors(Creature creature){
+        ArrayList<Creature> ancestors = new ArrayList<Creature>();
+        Creature current = creature.getParent();
+        while (current != null) {
+            ancestors.add(current);
+            current = current.getParent();
+        }
+        return ancestors;
+    }
 }
