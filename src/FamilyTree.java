3f0d43d9592cafa66686b64dd92380a316c882ac
Aarav Vishnoi <aaravvishnoi764@gmail.com>
2026-05-06 18:44:07 +1000

    Added getAncestors method to FamilyTree.java

--- src/FamilyTree.java
+++ src/FamilyTree.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+
 class FamilyTree {
     public int getDepth(Creature creature, int depth) {
         // Implementation for getting the depth of a creature in the family tree
@@ -7,24 +8,25 @@ class FamilyTree {
         if (creature.getParent() == null) {
             return depth;
         }
-        return getDepth(creature.getParent(), depth+1);
+        return getDepth(creature.getParent(), depth + 1);
     }
 
-
-    public String printLineage(Creature creature){
-        //should recurisvely print each anecestor name up the chain
+    public String printLineage(Creature creature) {
+        // should recurisvely print each anecestor name up the chain
         if (creature.getParent() == null) {
             return creature.getName();
         }
         return creature.getName() + " -> " + printLineage(creature.getParent());
     }
 
-    public ArrayList<Creature> getAncestors(Creature creature){
+    public ArrayList<Creature> getAncestors(Creature creature) {
         ArrayList<Creature> ancestors = new ArrayList<Creature>();
         Creature current = creature.getParent();
-        while (current != null) {
+        if (current == null) {
+            return ancestors;
+        } else {
             ancestors.add(current);
-            current = current.getParent();
+            ancestors.addAll(getAncestors(current));
         }
         return ancestors;
     }
