d92ea8b1c39055b3ab7b5a7d852c66e35a77e9eb
Aarav Vishnoi <aaravvishnoi764@gmail.com>
2026-05-11 16:10:31 +1000

    Added inline commnets to FamilyTree.java

--- src/FamilyTree.java
+++ src/FamilyTree.java
@@ -4,7 +4,7 @@ class FamilyTree {
     public int getDepth(Creature creature, int depth) {
         // Implementation for getting the depth of a creature in the family tree
         // base case if((c.getParent() != null, continue))
-        // recursive case: call getDepht again but on c.getParent() and increment by 1;
+        // recursive case: call getDepth again but on c.getParent() and increment by 1;
         if (creature.getParent() == null) {
             return depth;
         }
@@ -18,13 +18,15 @@ class FamilyTree {
         }
         return creature.getName() + " -> " + printLineage(creature.getParent());
     }
-
+    //recursively adds all the ancestors to a new ArrayList with datatype Creature 
     public ArrayList<Creature> getAncestors(Creature creature) {
         ArrayList<Creature> ancestors = new ArrayList<Creature>();
         Creature current = creature.getParent();
+        //checks if there is no parent, then the Creature is the only ancestor in the family tree
         if (current == null) {
             return ancestors;
         } else {
+        //Adds the ancestors including the current creature and its ancestors to the Array
             ancestors.add(current);
             ancestors.addAll(getAncestors(current));
         }
