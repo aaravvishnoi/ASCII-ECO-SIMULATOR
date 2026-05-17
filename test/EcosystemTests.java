8a31e7da371785f012f7fc8c376470d91bd7e96f
Aarav Vishnoi <aaravvishnoi764@gmail.com>
2026-05-17 14:49:58 +1000

    Added for Junit test cases for ecosystem and added comments which were missing

--- test/EcosystemTests.java
+++ test/EcosystemTests.java
@@ -9,6 +9,11 @@ import ecosystem.Species;
 import ecosystem.FamilyTree;
 
 public class EcosystemTests {
+
+    // =================================================================
+    // Population Class Tests
+    // =================================================================
+
     @Test
     public void testMutateUpperBound() {
         // checking that mutation does not exceed upper bound
@@ -28,22 +33,15 @@ public class EcosystemTests {
     }
 
     @Test
-    public void testFamilyTreeDepth() {
-        // ensure getDepth returns a non-negative depth for a creature
-        Species testSpecies = new Species("TestSpecies", "predator", 20, 5, 100);
-        Creature testCreature = new Creature(1, 1, 1, 1, null, "Test", 1, testSpecies);
-        FamilyTree tree = new FamilyTree();
-        int depth = tree.getDepth(testCreature, 0);
-        assertEquals(0, depth);
-    }
+    public void testMutationAttempt() {
+        // checking Mutation rate 0 (no mutation) and 1 (always mutation attempt)
+        Species testSpecies = new Species("Test Species", null, 0, 0, 0);
+        Population testPopulation = new Population(testSpecies);
+        int result = testPopulation.mutate(0);
+        int result2 = testPopulation.mutate(1);
+        assertTrue(result == 0);
+        assertTrue(result2 >= 0);
 
-    @Test
-    public void testFamilyDepth() {
-        Creature parentCreature = new Creature(1, 1, 1, 1, null, "null", 0, null);
-        Creature childCreature = new Creature(0, 0, 0, 0, parentCreature, null, 0, null);
-        FamilyTree tree = new FamilyTree();
-        int depth = tree.getDepth(childCreature, 0);
-        assertEquals(1, depth);
     }
 
     @Test
@@ -61,6 +59,82 @@ public class EcosystemTests {
     }
 
     @Test
+    public void testReproduce() {
+        // Creature with health exactly 60 reproduces
+        Species testSpecies = new Species("Test", "predator", 10, 1, 50);
+        Population testPopulation = new Population(testSpecies);
+
+        Creature creature1 = new Creature(5, 5, 5, 5, null, "", 60, testSpecies);
+        testPopulation.getAllCreatures().add(creature1);
+        testPopulation.reproduce();
+        // the creatures reproduce asexually
+        assertEquals(2, testPopulation.getPopulationCount());
+
+        //Checking if creature with health less then 60 reproduces 
+        Species testSpecies2 = new Species("Test2", "prey", 0, 0, 0);
+        Population testPopulation2 = new Population(testSpecies2);
+        Creature creature2 = new Creature(0, 0, 0, 0, creature1, null, 59, testSpecies2);
+        testPopulation2.getAllCreatures().add(creature2);
+        testPopulation2.reproduce();
+        assertEquals(1, testPopulation2.getPopulationCount());
+    }
+
+    @Test
+    public void testCarryingCapacity() {
+        //checking if reproduction still happens if carryingCapacity has been reached.
+        Species testSpecies = new Species("test", "Prey", 0, 0, 1);
+        Population testPopulation = new Population(testSpecies);
+        Creature creature1 = new Creature(0, 0, 0, 0, null, "Test", 100, testSpecies);
+        testPopulation.getAllCreatures().add(creature1);
+        testPopulation.reproduce();
+        assertEquals(1, testPopulation.getPopulationCount());
+    }
+
+    @Test
+    public void testEmptyPopulation(){
+        //Ensuring an empty population will not reproduce
+        Species testSpecies = new Species("Test", null, 0, 0, 0);
+        Population testPopulation = new Population(testSpecies);
+        testPopulation.reproduce();
+        assertEquals(0,testPopulation.getPopulationCount());
+    }
+
+    @Test
+    public void testPlantReproduction(){
+        //Ensuring Plant population does not reproduce
+        Species plantSpecies = new Species("Plant", null, 0, 0, 0);
+        Population testPopulation = new Population(plantSpecies);
+        Creature planCreature = new Creature(0, 0, 0, 0, null, "Plant", 0, plantSpecies);
+        testPopulation.getAllCreatures().add(planCreature);
+        testPopulation.reproduce();
+        assertEquals(1,testPopulation.getPopulationCount());
+    }
+
+    // =================================================================
+    // FamilyTree Class Tests
+    // =================================================================
+
+    @Test
+    public void testFamilyTreeDepth() {
+        // ensure getDepth returns a non-negative depth for a creature
+        Species testSpecies = new Species("TestSpecies", "predator", 20, 5, 100);
+        Creature testCreature = new Creature(1, 1, 1, 1, null, "Test", 1, testSpecies);
+        FamilyTree tree = new FamilyTree();
+        int depth = tree.getDepth(testCreature, 0);
+        assertEquals(0, depth);
+    }
+
+    @Test
+    public void testFamilyDepth() {
+        // ensure getDepth returns the correct depth for a creature with a parent
+        Creature parentCreature = new Creature(1, 1, 1, 1, null, "null", 0, null);
+        Creature childCreature = new Creature(0, 0, 0, 0, parentCreature, null, 0, null);
+        FamilyTree tree = new FamilyTree();
+        int depth = tree.getDepth(childCreature, 0);
+        assertEquals(1, depth);
+    }
+
+    @Test
     public void testLineage() {
         // create parent and child and check returned string contains both names with
         // "->" between them
@@ -73,26 +147,12 @@ public class EcosystemTests {
 
     @Test
     public void testGetAncestors() {
+        // ensure getAncestors returns the correct number of ancestors
         Creature creature1 = new Creature(0, 0, 0, 0, null, null, 0, null);
         Creature creature2 = new Creature(0, 0, 0, 0, creature1, null, 0, null);
         Creature creature3 = new Creature(0, 0, 0, 0, creature2, null, 0, null);
         FamilyTree tree = new FamilyTree();
         ArrayList<Creature> ancestors = tree.getAncestors(creature3);
-        assertEquals(2,ancestors.size());
+        assertEquals(2, ancestors.size());
     }
-
-    @Test
-    public void testReproduce(){
-        Species testSpecies = new Species("Test", "predator", 10, 1, 50);
-        Population testPopulation = new Population(testSpecies);
-
-        Creature deadCreature = new Creature(5, 5, 5, 5, null, "DeadOne", 60, testSpecies);
-        testPopulation.getAllCreatures().add(deadCreature);
-        testPopulation.reproduce();
-        //the creatures reproduce asexually 
-        assertEquals(2,testPopulation.getPopulationCount());
-    }
-
-    
-
 }
